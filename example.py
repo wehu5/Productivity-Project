8a7f16fffd171807f3070cd99b17b7318066bf93
Weiyu_H <weiyu.hu21@gmail.com>
2022-10-28 16:16:20 -0700

    initial commit

---
+++ example.py
@@ -0,0 +1,48 @@
+"""
+Demonstration of the GazeTracking library.
+Check the README.md for complete documentation.
+"""
+
+import cv2
+from gaze_tracking import GazeTracking
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+
+while True:
+    # We get a new frame from the webcam
+    _, frame = webcam.read()
+
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(frame)
+
+    frame = gaze.annotated_frame()
+    frame = cv2.flip(frame, 1)
+    text = ""
+
+    if gaze.is_blinking():
+        text = "Blinking"
+    elif gaze.is_right():
+        text = "Looking right"
+    elif gaze.is_left():
+        text = "Looking left"
+    elif gaze.is_center():
+        text = "Looking center"
+
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+    cv2.imshow("Demo", frame)
+
+    k = cv2.waitKey(1) & 0xFF
+    if not (cv2.getWindowProperty('Demo',cv2.WND_PROP_VISIBLE)):   
+        break
+    elif cv2.waitKey(1) == 27:
+        break
+   
+webcam.release()
+cv2.destroyAllWindows()
