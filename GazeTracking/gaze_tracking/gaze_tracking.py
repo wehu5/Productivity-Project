c5cb6891842feb633cfa4c7641e7070e61956fbf
Weiyu_H <weiyu.hu21@gmail.com>
2022-11-05 15:58:40 -0700

    close with released cam

--- GazeTracking/gaze_tracking/gaze_tracking.py
+++ GazeTracking/gaze_tracking/gaze_tracking.py
@@ -99,12 +99,12 @@ class GazeTracking(object):
     def is_up(self):
         """Returns true if the user is looking to the top"""
         if self.pupils_located:
-            return self.vertical_ratio() >= 0.4
+            return self.vertical_ratio() <= 0.4
 
     def is_down(self):
         """Returns true if the user is looking to the down"""
         if self.pupils_located:
-            return self.vertical_ratio() <= 0.8
+            return self.vertical_ratio() >= 0.6
 
     def is_right(self):
         """Returns true if the user is looking to the right"""
@@ -125,7 +125,7 @@ class GazeTracking(object):
         """Returns true if the user closes his eyes"""
         if self.pupils_located:
             blinking_ratio = (self.eye_left.blinking + self.eye_right.blinking) / 2
-            return blinking_ratio > 5
+            return blinking_ratio > 3
 
     def annotated_frame(self):
         """Returns the main frame with pupils highlighted"""
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response
 from imutils.video import VideoStream
-import imutils
+import threading
 import os
 from pathlib import Path
 from GazeTracking.gaze_tracking import GazeTracking
@@ -11,57 +11,52 @@ path_ = os.path.join(parent_path, "GazeTracking/example.py")
 gaze = GazeTracking()
 
 ### start
-
-# camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')  # use 0 for web camera
-camera = cv2.VideoCapture(0)  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
-
-frame_rate = 30
-frame_height = int(camera.get(4))
-frame_width = int(camera.get(3))
-frame_counter = 0
-
-fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 start = False
-
 
 def gen_frames():  # generate frame by frame from camera
     print('gen_frames')
-    while True:
+    global start
+    camera = cv2.VideoCapture(0)  # use 0 for web camera
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    frame_rate = 30
+    frame_height = int(camera.get(4))
+    frame_width = int(camera.get(3))
+    frame_counter = 0
+    while start:
         # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            continue
-        else:
-          gaze.refresh(frame)
-          frame = gaze.annotated_frame()
-          frame = cv2.flip(frame, 1)
-          text = ""
+      success, frame = camera.read()  # read the camera frame
+      if not success:
+        continue
+      else:
+        gaze.refresh(frame)
+        frame = gaze.annotated_frame()
+        frame = cv2.flip(frame, 1)
+        text = ""
 
-          if gaze.is_blinking():
-            text = "Blinking"
-          elif gaze.is_right():
-            text = "Looking right"
-          elif gaze.is_left():
-            text = "Looking left"
-          elif gaze.is_up():
-            text = "Looking Up"
-          elif gaze.is_down():
-            text = "Looking Down"
-          elif gaze.is_center():
-            text = "Looking center"
+      if gaze.is_blinking():
+        text = "Blinking"
+      elif gaze.is_right():
+        text = "Looking right"
+      elif gaze.is_left():
+        text = "Looking left"
+      elif gaze.is_up():
+        text = "Looking Up"
+      elif gaze.is_down():
+        text = "Looking Down"
+      elif gaze.is_center():
+        text = "Looking center"
 
-          cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+      cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
-          left_pupil = gaze.pupil_left_coords()
-          right_pupil = gaze.pupil_right_coords()
-          cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-          cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-          ret, buffer = cv2.imencode('.jpg', frame)
-          frame = buffer.tobytes()
-          yield (b'--frame\r\n'
+      left_pupil = gaze.pupil_left_coords()
+      right_pupil = gaze.pupil_right_coords()
+      cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+      cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+      ret, buffer = cv2.imencode('.jpg', frame)
+      frame = buffer.tobytes()
+      yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    camera.release()
 
 ### end
 
@@ -69,12 +64,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  global start
+  start = False
   return render_template('index.html')
 
 @app.route('/my-link/')
 def my_link():
   #webcam = cv2.VideoCapture(0)
   #_, frame = webcam.read()
+  global start
+  start = True
   return render_template('my-link.html')
 
 @app.route('/video_feed')
